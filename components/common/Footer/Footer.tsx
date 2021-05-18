029d42e7011b03af875fc3e1702152c9910db406
nnhhiilliisstt <kumar.aditya221b@email.com>
2021-05-18 16:17:13 +0530

    file

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -31,7 +31,7 @@ const Footer: FC<Props> = ({ className, pages }) => {
                 <span className="rounded-full border border-gray-700 mr-2">
                   <Logo />
                 </span>
-                <span>ACME</span>
+                <span>E-commerce</span>
               </a>
             </Link>
           </div>
@@ -86,7 +86,7 @@ const Footer: FC<Props> = ({ className, pages }) => {
             <div className="flex space-x-6 items-center h-10">
               <a
                 aria-label="Github Repository"
-                href="https://github.com/vercel/commerce"
+                href="https://github.com/nnhhiilliisstt"
                 className={s.link}
               >
                 <Github />
@@ -97,11 +97,11 @@ const Footer: FC<Props> = ({ className, pages }) => {
         </div>
         <div className="py-12 flex flex-col md:flex-row justify-between items-center space-y-4">
           <div>
-            <span>&copy; 2020 ACME, Inc. All rights reserved.</span>
+            <span>&copy; 2020 Kumar Aditya, Inc. All rights reserved.</span>
           </div>
           <div className="flex items-center text-primary">
             <span className="text-primary">Crafted by</span>
-            <a
+            {/* <a
               rel="noopener"
               href="https://vercel.com"
               aria-label="Vercel.com Link"
@@ -112,7 +112,7 @@ const Footer: FC<Props> = ({ className, pages }) => {
                 className="inline-block h-6 ml-4 text-primary"
                 alt="Vercel.com Logo"
               />
-            </a>
+            </a> */}
           </div>
         </div>
       </Container>
